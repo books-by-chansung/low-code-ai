b42aa29f88789a911675dc362a83afd9c91e68a5
Chansung Park <deep.diver.csp@gmail.com>
2024-10-28 12:10:04 +0900

    Update bqml_tuning.sql

--- chapter8/bqml_tuning.sql
+++ chapter8/bqml_tuning.sql
@@ -1,4 +1,4 @@
-## SQL statement in BigQuery to create dataset and load data into tables. Directions for using Console are in Chapter 8
+## 데이터셋을 생성하고 테이블로 불러오기 위한, 빅쿼리용 SQL입니다. 콘솔을 사용하는 방법은 8장을 참고하기 바랍니다
 
 CREATE SCHEMA car_sales_prices OPTIONS(location='US');
 
@@ -20,7 +20,7 @@ LOAD DATA OVERWRITE cars_sales_prices.car_prices_test
     uris = ['gs://low-code-ai/chapter_8/car_prices_test.csv']
   );
   
-## SQL statement to preprocess and explore a few rows of data.
+## 데이터의 일부 행을 탐색하고 전처리하기 위한 SQL 문
 
 SELECT
   * EXCEPT (int64_field_0, mmr, odometer, year, condition),
@@ -33,7 +33,7 @@ FROM
   `car_sales_prices.car_prices_train`
 LIMIT 10;
 
-## CREATE MODEL statement for a linear model using the TRANSFORM statement.
+## TRANSFORM 문을 곁들여 선형 모델을 생성하는 MODEL 문
 
 CREATE OR REPLACE MODEL
   `car_sales_prices.linear_car_model` 
@@ -53,19 +53,19 @@ SELECT
 FROM
   `car_sales_prices.car_prices_train`;
 
-## ML.EVALUATE statement for linear_car_model.
+## linear_car_model 모델에 대한 ML.EVALUATE 문
 
 SELECT mean_absolute_error
 FROM ML.EVALUATE(MODEL `ddml.linear_car_model`,
     (SELECT * FROM `car_sales_prices.car_prices_valid`))
 
-## ML.PREDICT statement for linear_car_model.
+## linear_car_model 모델에 대한 ML.PREDICT 문
 
 SELECT *
 FROM ML.PREDICT(MODEL `ddml.linear_car_model`,
     (SELECT * FROM `car_sales_prices.car_prices_valid`));
     
-## CREATE MODEL statement for DNN model.
+## DNN 모델을 생성하기 위한 CREATE MODEL 문
 
 CREATE OR REPLACE MODEL
   `car_sales_prices.dnn_car_model` 
@@ -86,7 +86,7 @@ SELECT
 FROM
   `car_sales_prices.car_prices_train`;
 
-## Hyperparameter tuning job for DNN in BigQuery ML.
+## 빅쿼리 ML의 DNN을 위한 하이퍼파라미터 튜닝 작업
 
 CREATE OR REPLACE MODEL
   `car_sales_prices.dnn_hp_car_model` 
@@ -112,7 +112,7 @@ FROM
   `car_sales_prices.car_prices_train`;
 
 
-## Query using ML.TRIAL_INFO to see evaluation metrics for candidate models
+## 후보 모델에 대한 평가 지표를 확인하기 위한 ML.TRIAL_INFO 함수 사용
 
 SELECT
   *
